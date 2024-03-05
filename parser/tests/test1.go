cd6d58c36609d31537f9c35a942136827a7c0a52
Elizabeth <meera@gmail.com>
2024-03-05 22:18:08 +0530

    file structure changed

---
+++ parser/tests/test1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    var a string
+    a = "string"
+    fmt.Println(a)
+
+    var b, c, d int
+    b = 1
+    c = 2
+    d = b + c
+    fmt.Println(d)$
+    /* hello
+    and hello
+    and hello  */
+
+}
---
+++ parser/tests/test3.go
@@ -0,0 +1,10 @@
+package main
+
+func add(a, b int) int {
+    return a + b
+}
+
+func main() {
+    resultlskdjflsdkjflkjdslkfjsdljflsdjflsdkfjlesdjflsdjdfokjr89ii68ok := add(10, 20)
+    println("Result:", resultlskdjflsdkjflkjdslkfjsdljflsdjflsdkfjlesdjflsdjdfokjr89ii68ok)
+}
