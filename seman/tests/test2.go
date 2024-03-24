8cec1f69754f0f1c0adac6a30ae4e0b8502132e3
harta <hammock073@gmail.com>
2024-03-24 19:18:49 +0530

    add: init seman

---
+++ seman/tests/test2.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+    var a int = 10
+    var b int = 20
+    if a < b {
+        println("a is less than b")
+    } else {
+        println("a is greater than or equal to b)
+    }
+}
---
+++ seman/tests/test3.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+    var a int = 10
+    var b int = 20
+
+    resultl = a + b
+    println("Result:")
+    println(resultl)
+}
